82732548ec0af696b0456cb2bd3ece8778791dd0
Adsandro <adsandroxerd@gmail.com>
2023-09-04 12:11:23 -0300

    Implement Log

--- code/main.py
+++ code/main.py
@@ -5,6 +5,7 @@ import logging
 host_prefix = '192.168.0.'
 users = open('code\\users.txt', 'r')
 passwords = open('code\\passwords.txt', 'r')
+logging.basicConfig(level=logging.INFO, filename="log\\program.log", format="%(asctime)s - %(levelname)s - %(message)s")
 
 def sshAcess(passwords, users, host_prefix):
     # Iterar sobre uma faixa de endereços IP
@@ -22,7 +23,7 @@ def sshAcess(passwords, users, host_prefix):
                     ssh.connect(ip, username=user, password=password, timeout=3)
                     
                     # Se a conexão for bem-sucedida, exibir uma mensagem
-                    print(f'{ip} Connected!')
+                    logging.info(f'{ip}:{user} Connected!')
                     
                     # Fechar a conexão SSH
                     ssh.close()
@@ -37,7 +38,7 @@ def sshAcess(passwords, users, host_prefix):
                     
                 except Exception as e:
                     # Se ocorrer uma exceção não tratada, exibir uma mensagem de erro genérica
-                    print(f'{ip} Error: {str(e)}')
+                    print(f'{ip}:{user}Error: {str(e)}')
 
 if __name__ == "__main__":
     sshAcess(passwords, users, host_prefix)
